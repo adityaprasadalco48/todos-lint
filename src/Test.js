2b0b0fb83af9518ad8f0aaf093b4c5082002941f
sudha singh <imsudha13@gmail.com>
2021-06-17 14:15:31 +0530

    testing husky

---
+++ src/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Testing() {
+  console.log('hello husky');
+  return (
+    <div>
+      <h2>Testing for Husky</h2>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Testing from './Test';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Testing />
   </React.StrictMode>,
   document.getElementById('root')
 );
