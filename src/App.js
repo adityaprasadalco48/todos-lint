7af08304a37db5f33c50b88c6d8c94836e14e9ba
sudha singh <imsudha13@gmail.com>
2021-06-17 14:55:12 +0530

    some changes for husky

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 // import logo from './logo.svg';
 
 function App() {
-                    const [name, setName] = useState('Namo');
-                    // console.log(name);
+  const [name, setName] = useState('Namo')
+  console.log(name)
 
-                    useEffect(() => {
-                      console.log('I have Done This');
-                    }, []);
+  useEffect(() => {
+    console.log('I have Done This')
+  }, [])
 
-                    return (
-                      <div className="App">
-                        <h1>New Here! Wanna meet Developer ?</h1>
-                      </div>
-                    );
+  return (
+    <div className="App">
+      <h1>New Here! Wanna meet Developer ?</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/Test.js
+++ src/Test.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 export default function Testing() {
-  console.log('hello husky');
+  console.log('hello husky')
+  console.log('try to do husky')
   return (
     <div>
       <h2>Testing for Husky</h2>
     </div>
-  );
+  )
 }
