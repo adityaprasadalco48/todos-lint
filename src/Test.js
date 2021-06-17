515296e2b9ca20c943020574bc90ef6c0eff6de8
adityaprasad-48 <adityaprasad20799@gmail.com>
2021-06-17 16:35:44 +0530

    new here

--- src/Test.js
+++ src/Test.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
 export default function Testing() {
-    var huskyItem = ['husky', 'eslint', 'prettier'];
-    console.log('hello husky');
-    console.log('try to do husky');
-    return (
-        <div>
-            {huskyItem.map((item, index) => (
-                <>
-                    <h2 key={index}>
-                      Testing for Husky = {item
-                      }</h2>
-                </>
-            ))}
-        </div>
-    );
+  var huskyItem = ['husky', 'eslint', 'prettier'];
+  console.log('hello husky');
+  console.log('try to do husky');
+  return (
+    <div>
+      {huskyItem.map((item, index) => (
+        <>
+          <h2 key={index}>Testing for Husky = 
+          {item}</h2>
+        </>
+      ))}
+    </div>
+  );
 }
